8dda864532adb3d10ec97780449502d486e71dea
pekkamakkonen <pek.makkonen@gmail.com>
2021-01-09 02:51:21 +0200

    Get gamedata of three season

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 
 
 class Game:
@@ -16,6 +17,44 @@ class Game:
                 game_ids.append(game["gamePk"])
         return game_ids
 
+    def get_game_data(self):
+        game_ids = self.get_game_ids()
 
-games = Game("20172018")
-games.get_game_ids()
+        with sqlite3.connect("nhldb.sqlite3") as conn:
+
+            for game_id in game_ids:
+                url = f"https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live"
+                response = requests.get(url)
+                game_data = response.json()["gameData"]
+                game = game_data["game"]
+                date_time = game_data["datetime"]
+                status = game_data["status"]
+                venue = game_data["venue"]
+
+                conn.execute("INSERT INTO Game VALUES (?, ?, ?, ?, ?, ?, ?)", (
+                             game.get("pk"),
+                             game.get("season"),
+                             game.get("type"),
+                             date_time.get("dateTime"),
+                             date_time.get("endDateTime"),
+                             status.get("statusCode"),
+                             venue.get("name")
+                             )
+                             )
+            conn.commit()
+
+          #  games.append(game)
+         #   print(game)
+      #  return games
+
+ #   def save_games(self):
+ #       games = self.get_game_data()
+
+#        for game in games:
+ #           print(game)
+            # print(response.text)
+
+
+games = Game("20192020")
+# games.get_game_ids()
+games.get_game_data()
